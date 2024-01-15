fe2fdff49e75271729e881f869ca174b7a929924
Joshua <78570527+1travelintexan@users.noreply.github.com>
2024-01-15 05:28:06 +0100

    day 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    //correct way to shuffle
+    for (let i = 0; i < this.choices.length; i++) {
+      let randomIndex = Math.floor(Math.random() * this.choices.length);
+      let choiceToMove = this.choices[i];
+      this.choices.splice(i, 1);
+      this.choices.splice(randomIndex, 0, choiceToMove);
+    }
+  }
 }
